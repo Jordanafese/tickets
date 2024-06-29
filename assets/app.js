2795be391a86a998d55a4ba86f749f76a9c1be8f
Enoch Asuwemhe <kingenoch5@gmail.com>
2024-06-29 07:50:26 +0100

    changes

--- assets/app.js
+++ assets/app.js
@@ -43,7 +43,7 @@ function clear_modal() {
 
 
 
-var deadline = new Date(2024,4,12,0,30,0,0).getTime();
+var deadline = new Date("July 13, 2024 12:00:00").getTime();
   
 var x = setInterval(function() {
   
